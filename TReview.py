0e6bd1620ea95e9584a2bee44600961bc5f389e1
Lucius <LuciusPrivate@163.com>
2020-09-09 20:27:47 +0800

    Update TReview.py

--- TReview.py
+++ TReview.py
@@ -1,151 +1,139 @@
-# 76
+# 5
 class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        start, left, right, minLen, match = 0, 0, 0, float('inf'), 0
-        window = {}
-        needs = dict((i, t.count(i)) for i in t)
-
-        while right < len(s):
-            c1 = s[right]
-            if c1 in needs.keys():
-                window[c1] = window.get(c1, 0) + 1
-                if window[c1] == needs[c1]:
-                    match += 1
-            right += 1
-
-            while match == len(needs):
-                if right - left < minLen:
-                    start = left
-                    minLen = right - left
-                c2 = s[left]
-                if c2 in needs.keys():
-                    window[c2] -= 1
-                    if window[c2] < needs[c2]:
-                        match -= 1
-                left += 1
-        return '' if minLen == float('inf') else s[start:start + minLen]
-
-# 438
-class Solution:
-    def findAnagrams(self, s: str, p: str) -> List[int]:
-        res = []
-        left, right, match = 0, 0, 0
-        window = {}
-        needs = dict((i, p.count(i)) for i in p)
-
-        while right < len(s):
-            c1 = s[right]
-            if c1 in needs.keys():
-                window[c1] = window.get(c1, 0) + 1
-                if window[c1] == needs[c1]:
-                    match += 1
-            right += 1
-
-            while match == len(needs):
-                if right - left == len(p):
-                    res.append(left)
-                c2 = s[left]
-                if c2 in needs.keys():
-                    window[c2] -= 1
-                    if window[c2] < needs[c2]:
-                        match -= 1
-                left += 1
-        return res
-
-# 3
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        right, left, res = 0, 0, 0
-        window = {}
-
-        while right < len(s):
-            c1 = s[right]
-            window[c1] = window.get(c1, 0) + 1
-            right += 1
-            while window[c1] > 1:
-                c2 = s[left]
-                window[c2] -= 1
-                left += 1
-            res = max(res, right - left)
-        return res
-
-# 567
-class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        needs = dict((i, s1.count(i)) for i in s1)
-        right, left, match = 0, 0, 0
-        window = {}
-
-        while right < len(s2):
-            c1 = s2[right]
-            if c1 in needs.keys():
-                window[c1] = window.get(c1, 0) + 1
-                if window[c1] == needs[c1]:
-                    match += 1
-            right += 1
-
-            while match == len(needs):
-                if right - left == len(s1):
-                    return True
-                c2 = s2[left]
-                if c2 in needs.keys():
-                    window[c2] -= 1
-                    if window[c2] < needs[c2]:
-                        match -= 1
-                left += 1
-        return False
-
-# 322
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        import functools
-        @functools.lru_cache(amount)
-        def dp(rem):
-            if rem < 0:#除不尽的返回负值
-                return -1
-            if rem == 0: #确保除尽
-                return 0
-            mini = int(1e9)
-            for coin in self.coins:
-                res = dp(rem - coin)#自顶向下
-                if res >= 0 and res < mini:#此处确保mini是最小
-                    mini = res + 1
-            return mini if mini < int(1e9) else -1#确保除不尽的始终返回为-1
+    def longestPalindrome(self, s: str) -> str:
+        size = len(s)
+        if size < 2:
+            return s
         
-        self.coins = coins
-        if amount < 1: 
-            return 0
-        return dp(amount)
+        dp = [[False for _ in range(size)] for _ in range(size)]
+
+        max_len = 1
+        start = 0
+
+        for i in range(size):
+            dp[i][i] = True
+        
+        for j in range(1, size):
+            for i in range(0, j):
+                dp[i][j] = (s[i] == s[j]) and (j - i < 3 or dp[i + 1][j - 1])
+                # if s[i] == s[j]:
+                #     if j - i < 3:
+                #         dp[i][j] = True
+                #     else:
+                #         dp[i][j] = dp[i + 1][j - 1]
+                # else:
+                #     dp[i][j] = False
+                
+                if dp[i][j]:
+                    cur_len = j - i + 1
+                    if cur_len > max_len:
+                        max_len = cur_len
+                        start = i
+        return s[start:start + max_len]
+
+    def longestPalindrome1(self, s: str) -> str:
+        size = len(s)
+        if size < 2:
+            return s
+        
+        max_len = 1
+        res = s[0]
+        for i in range(size):
+            palindrome_odd, odd_len = self.__center_spread(s, size, i, i)
+            palindrome_even, even_len = self.__center_spread(s, size, i, i + 1)
+
+            cur_max_sub = palindrome_odd if odd_len > even_len else palindrome_even
+            if max(cur_max_sub) > max_len:
+                max_len = len(cur_max_sub)
+                res = cur_max_sub
+        return res
     
-    def coinChange1(self, coins: List[int], amount: int) -> int:#解法可以与跳到目的的题目很像，但却不能用，很麻烦
-        dp = [float('inf')] * (amount + 1)
-        dp[0] = 0
+    def __center_spread(self, s, size, left, right):
+        i = left
+        j = right
+        while i >= 0 and j < size and s[i] == s[j]:
+            i -= 1
+            j += 1
+        return s[i + 1:j], j - i - 1
 
-        for coin in coins:#对每个coin进行遍历
-            for x in range(coin, amount + 1):#对coin从小到大更新，小的就更新，一样的或者大的就不更新
-                dp[x] = min(dp[x], dp[x - coin] + 1)
-        return dp[amount] if dp[amount] != float('inf') else -1
-
-# 122
+# 680
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        for i in range(1, len(prices)):
-            tmp = prices[i] - prices[i-1]
-            if tmp > 0:
-                profit += tmp
-        return profit
+    def validPalindrome(self, s: str) -> bool:
+        def checkPalindrome(low, high):
+            i, j = low, high
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True
+        
+        low, high = 0, len(s) - 1
+        while low < high:
+            if s[low] == s[high]:
+                low += 1
+                high -= 1
+            else:
+                return checkPalindrome(low + 1, high) or checkPalindrome(low, high - 1)
 
-    def maxProfit1(self, prices: List[int]) -> int:
-        return sum(b - a for a, b in zip(prices, prices[1:]) if b > a)
+# 190
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(31, -1, -1):
+            res |= (((n >> (31 - i)) & 1) << i)
+        
+        return res
 
-    def maxProfit2(self, prices: List[int]) -> int:
-        if not prices:
-            return 0
-        n = len(prices)
-        dp_i_0, dp_i_1 = 0, float('-inf')
-        for i in range(n):
-            tmp = dp_i_0
-            dp_i_0 = max(dp_i_0, dp_i_1 + prices[i])
-            dp_i_1 = max(dp_i_1, tmp - prices[i])
-        return dp_i_0
+    def reverseBits1(self, n: int) -> int:
+        res = 0
+        count = 0
+        while count < 32:
+            res <<= 1
+            res |= n & 1
+            n >>= 1
+            count += 1
+        
+        return res
+
+# 231
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n > 0 and n & n - 1 == 0
+
+# 455
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        res = 0
+        g.sort()
+        s.sort()
+
+        g_length = len(g)
+        s_length = len(s)
+
+        i = 0
+        j = 0
+        while i < g_length and j < s_length:
+            if g[i] <= s[j]:
+                res += 1
+                i += 1
+                j += 1
+            else:
+                j += 1
+
+        return res
+
+# 55
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if nums == [0]:
+            return True
+        maxDist = 0
+        end_index = len(nums) - 1
+        for i, jump in enumerate(nums):
+            if maxDist >= i and i + jump > maxDist:#最远距离大过终点index
+                maxDist = i + jump
+                if maxDist >= end_index:
+                    return True
+        
+        return False
