4a75ffe0f19172a2db2d745315721f4498c3da4d
Lucius <LuciusPrivate@163.com>
2020-09-14 19:56:16 +0800

    today

--- TReview.py
+++ TReview.py
@@ -1,162 +1,150 @@
-# rabin-karp
+# 394
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        d = 256
-        q = 9997
-        n = len(haystack)
-        m = len(needle)
-        h = pow(d, m - 1) % q
-        p = 0
-        t = 0
-
-        if m > n:
-            return -1
-
-        for i in range(m):
-            p = (d * p + ord(needle[i])) % q
-            t = (d * t + ord(haystack[i])) % q
-        for s in range(n - m + 1):
-            if p == t:
-                match = True
-                for i in range(m):
-                    if needle[i] != haystack[s + i]:
-                        match = False
-                        break
-                if match:
-                    return s
-            if s < n - m:
-                t = (t - h * ord(haystack[s])) % q
-                t = (t * d + ord(haystack[s + m])) % q
-                t = (t + q) % q
-        return -1
-
-# 205
-class Solution:
-    def isIsomorphic(self, s: str, t: str) -> bool:
-        dict = {}
-        for i in range(len(s)):
-            if s[i] not in dict.keys():
-                if t[i] in dict.values():
-                    return False
-                dict[s[i]] = t[i]
+    def decodeString(self, s: str) -> str:
+        stack, res, multi = [], "", 0
+        for c in s:
+            if c == '[':
+                stack.append([multi, res])
+                res, multi = "", 0
+            elif c == ']':
+                cur_multi, last_res = stack.pop()
+                res = last_res + cur_multi * res
+            elif '0' <= c <= '9':
+                multi = multi * 10 + int(c)            
             else:
-                if dict[s[i]] != t[i]:
-                    return False
-        return True
-
-# 44
-class Solution:
-    def isMatch(self, s: str, p: str) -> bool:
-        nrow = len(p) + 1
-        ncol = len(s) + 1
-
-        dp = [[False for _ in range(ncol)] for _ in range(nrow)]
-        dp[0][0] = True
-
-        if p.startswith("*"):
-            dp[1] = [True] * ncol
-
-        for m in range(1, nrow):
-            path = False
-            for n in range(1, ncol):
-                if p[m - 1] == "*":
-                    if dp[m - 1][0] == True:
-                        dp[m] = [True] * ncol
-                    if dp[m - 1][n] == True:
-                        path = True
-                    if path:
-                        dp[m][n] = True
-                elif p[m - 1] == "?" or p[m - 1] == s[n - 1]:
-                    dp[m][n] = dp[m - 1][n - 1]
-        return dp[-1][-1]
-
-# 10
-class Solution:
-    def isMatch(self, s: str, p: str) -> bool:
-        if not p:
-            return not s
-        if not s and len(p) == 1:
-            return False
-        
-        nrow = len(s) + 1
-        ncol = len(p) + 1
-
-        dp = [[False for c in range(ncol)] for _ in range(nrow)]
-        dp[0][0] = True
-        dp[0][1] = False
-        for c in range(2, ncol):
-            j = c - 1
-            if p[j] == "*":
-                dp[0][c] = dp[0][c - 2]
-        
-        for r in range(1, nrow):
-            i = r - 1
-            for c in range(1, ncol):
-                j = c - 1
-                if s[i] == p[j] or p[j] == ".":
-                    dp[r][c] = dp[r - 1][c - 1]
-                elif p[j] == "*":
-                    if p[j - 1] == s[i] or p[j - 1] == ".":
-                        dp[r][c] = dp[r - 1][c] or dp[r][c - 2]
-                    else:
-                        dp[r][c] = dp[r][c - 2]
-        return dp[-1][-1]
-
-# 5
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        size = len(s)
-        if size < 2:
-            return s
-        
-        dp = [[False for _ in range(size)] for _ in range(size)]
-
-        max_len = 1
-        start = 0
-
-        for i in range(size):
-            dp[i][i] = True
-        
-        for j in range(1, size):
-            for i in range(0, j):
-                dp[i][j] = (s[i] == s[j]) and (j - i < 3 or dp[i + 1][j - 1])
-                # if s[i] == s[j]:
-                #     if j - i < 3:
-                #         dp[i][j] = True
-                #     else:
-                #         dp[i][j] = dp[i + 1][j - 1]
-                # else:
-                #     dp[i][j] = False
-                
-                if dp[i][j]:
-                    cur_len = j - i + 1
-                    if cur_len > max_len:
-                        max_len = cur_len
-                        start = i
-        return s[start:start + max_len]
-
-    def longestPalindrome1(self, s: str) -> str:
-        size = len(s)
-        if size < 2:
-            return s
-        
-        max_len = 1
-        res = s[0]
-        for i in range(size):
-            palindrome_odd, odd_len = self.__center_spread(s, size, i, i)
-            palindrome_even, even_len = self.__center_spread(s, size, i, i + 1)
-
-            cur_max_sub = palindrome_odd if odd_len > even_len else palindrome_even
-            if max(cur_max_sub) > max_len:
-                max_len = len(cur_max_sub)
-                res = cur_max_sub
+                res += c
         return res
-    
-    def __center_spread(self, s, size, left, right):
-        i = left
-        j = right
-        while i >= 0 and j < size and s[i] == s[j]:
-            i -= 1
-            j += 1
-        return s[i + 1:j], j - i - 1
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        def dfs(s, i):
+            res, multi = "", 0
+            while i < len(s):
+                if '0' <= s[i] <= '9':
+                    multi = multi * 10 + int(s[i])
+                elif s[i] == '[':
+                    i, tmp = dfs(s, i + 1)
+                    res += multi * tmp
+                    multi = 0
+                elif s[i] == ']':
+                    return i, res
+                else:
+                    res += s[i]
+                i += 1
+            return res
+        return dfs(s,0)
+
+# 211
+class WordDictionary(object):
+    class Node:
+        def __init__(self):
+            self.is_word = False
+            self.next = dict()
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = WordDictionary.Node()
+
+    def addWord(self, word):
+        """
+        Adds a word into the data structure.
+        :type word: str
+        :rtype: void
+        """
+        cur_node = self.root
+        for alpha in word:
+            if alpha not in cur_node.next:
+                cur_node.next[alpha] = WordDictionary.Node()
+            cur_node = cur_node.next[alpha]
+        if not cur_node.is_word:
+            cur_node.is_word = True
+
+    def search(self, word):
+        """
+        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+        :type word: str
+        :rtype: bool
+        """
+        # 注意：这里要设置辅助函数
+        return self.match(self.root, word, 0)
+
+    def match(self, node, word, index):
+        if index == len(word):
+            return node.is_word
+        alpha = word[index]
+        if alpha == '.':
+            for next in node.next:
+                if self.match(node.next[next], word, index + 1):
+                    return True
+            # 注意：这里要返回
+            return False
+        else:
+            # 注意：这里要使用 else
+            if alpha not in node.next:
+                return False
+            # 注意：这里要使用 return 返回
+            return self.match(node.next[alpha], word, index + 1)
+
+# 231
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n > 0 and n & n - 1 == 0
+
+# 190
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(31, -1, -1):
+            res |= (((n >> (31 - i)) & 1) << i)
         
+        return res
+
+    def reverseBits1(self, n: int) -> int:
+        res = 0
+        count = 0
+        while count < 32:
+            res <<= 1
+            res |= n & 1
+            n >>= 1
+            count += 1
+        
+        return res
+
+# 55
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if nums == [0]:
+            return True
+        maxDist = 0
+        end_index = len(nums) - 1
+        for i, jump in enumerate(nums):
+            if maxDist >= i and i + jump > maxDist:#最远距离大过终点index
+                maxDist = i + jump
+                if maxDist >= end_index:
+                    return True
+        
+        return False
+
+# 455
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        res = 0
+        g.sort()
+        s.sort()
+
+        g_length = len(g)
+        s_length = len(s)
+
+        i = 0
+        j = 0
+        while i < g_length and j < s_length:
+            if g[i] <= s[j]:
+                res += 1
+                i += 1
+                j += 1
+            else:
+                j += 1
+
+        return res
