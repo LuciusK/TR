e0b060f379fe8253b2817c18c0fb159ccb2f76df
Lucius <LuciusPrivate@163.com>
2020-09-23 21:38:42 +0800

    Update TReview.py

--- TReview.py
+++ TReview.py
@@ -1,165 +1,125 @@
-# 394
+# 231
 class Solution:
-    def decodeString(self, s: str) -> str:
-        stack, res, multi = [], "", 0
-        for c in s:
-            if c == '[':
-                stack.append([multi, res])
-                res, multi = "", 0
-            elif c == ']':
-                cur_multi, last_res = stack.pop()
-                res = last_res + cur_multi * res
-            elif '0' <= c <= '9':
-                multi = multi * 10 + int(c)            
-            else:
-                res += c
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n > 0 and n & n - 1 == 0
+
+# 190
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(31, -1, -1):
+            res |= (((n >> (31 - i)) & 1) << i)
+        
         return res
 
-class Solution:
-    def decodeString(self, s: str) -> str:
-        def dfs(s, i):
-            res, multi = "", 0
-            while i < len(s):
-                if '0' <= s[i] <= '9':
-                    multi = multi * 10 + int(s[i])
-                elif s[i] == '[':
-                    i, tmp = dfs(s, i + 1)
-                    res += multi * tmp
-                    multi = 0
-                elif s[i] == ']':
-                    return i, res
-                else:
-                    res += s[i]
-                i += 1
-            return res
-        return dfs(s,0)
-
-# 211
-class WordDictionary(object):
-    class Node:
-        def __init__(self):
-            self.is_word = False
-            self.next = dict()
-
-    def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.root = WordDictionary.Node()
-
-    def addWord(self, word):
-        """
-        Adds a word into the data structure.
-        :type word: str
-        :rtype: void
-        """
-        cur_node = self.root
-        for alpha in word:
-            if alpha not in cur_node.next:
-                cur_node.next[alpha] = WordDictionary.Node()
-            cur_node = cur_node.next[alpha]
-        if not cur_node.is_word:
-            cur_node.is_word = True
-
-    def search(self, word):
-        """
-        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
-        :type word: str
-        :rtype: bool
-        """
-        # 注意：这里要设置辅助函数
-        return self.match(self.root, word, 0)
-
-    def match(self, node, word, index):
-        if index == len(word):
-            return node.is_word
-        alpha = word[index]
-        if alpha == '.':
-            for next in node.next:
-                if self.match(node.next[next], word, index + 1):
-                    return True
-            # 注意：这里要返回
-            return False
-        else:
-            # 注意：这里要使用 else
-            if alpha not in node.next:
-                return False
-            # 注意：这里要使用 return 返回
-            return self.match(node.next[alpha], word, index + 1)
-
-# 290
-class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
-        res = s.split()
-        return list(map(pattern.index, pattern)) == list(map(res.index, res))
-    
-
-# 389
-
-class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
-        return chr(sum(map(ord, t)) - sum(map(ord, s)))
-    
-    def findTheDifference1(self, s: str, t: str) -> str:
-        from collections import Counter
-        c1 = Counter(s)
-        c2 = Counter(t)
-        for i in range(ord("a"), ord("z") + 1):
-            tmp = chr(i)
-            if c2[tmp] - c1[tmp] == 1:
-                return tmp
-
-# 92
-
-class Solution: # a, c 包围这 m， n， 而b， d代表m， n
-    def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
-        if m == n:
-            return head
-        dummy = ListNode(-1)
-        dummy.next = head
-        a, d = dummy, dummy
-        for _ in range(m - 1):
-            a = a.next
-        for _ in range(n):
-            d = d.next        
-        b, c = a.next, d.next
-
-        # 开始逆转
-        pre = b
-        cur = pre.next
-
-        while cur != c:
-            next = cur.next
-            cur.next = pre
-            pre = cur
-            cur = next
-        a.next = d
-        b.next = c
-        return dummy.next
-
-# 61
-
-class Solution:
-    def rotateRight(self, head: ListNode, k: int) -> ListNode:
-        if not head:
-            return None
-        if not head.next:
-            return head
+    def reverseBits1(self, n: int) -> int:
+        res = 0
+        count = 0
+        while count < 32:
+            res <<= 1
+            res |= n & 1
+            n >>= 1
+            count += 1
         
-        old_tail = head
-        n = 1
-        while old_tail.next:
-            old_tail = old_tail.next
-            n += 1
-        old_tail.next = head
+        return res
 
-        # find new tail : (n - k % n - 1)th node
-        # and new head : (n - k % n)th node
-        new_tail = head
-        for i in range(n - k % n - 1):
-            new_tail = new_tail.next
-        new_head = new_tail.next
+# 55
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if nums == [0]:
+            return True
+        maxDist = 0
+        end_index = len(nums) - 1
+        for i, jump in enumerate(nums):
+            if maxDist >= i and i + jump > maxDist:#最远距离大过终点index
+                maxDist = i + jump
+                if maxDist >= end_index:
+                    return True
+        
+        return False
 
-        new_tail.next = None
+# 455
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        res = 0
+        g.sort()
+        s.sort()
 
-        return new_head
+        g_length = len(g)
+        s_length = len(s)
+
+        i = 0
+        j = 0
+        while i < g_length and j < s_length:
+            if g[i] <= s[j]:
+                res += 1
+                i += 1
+                j += 1
+            else:
+                j += 1
+
+        return res
+
+# 148
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        slow, fast = head, head.next
+        while fast and fast.next:
+            fast, slow = fast.next.next, slow.next 
+        midnext, slow.next = slow.next, None
+        left, right = self.sortList(head), self.sortList(midnext)
+
+        h = res = ListNode(0)
+        while left and right:
+            if left.val < right.val:
+                h.next, left = left, left.next 
+            else:
+                h.next, right = right, right.next 
+            h = h.next 
+        h.next = left if left else right 
+        return res.next
+
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        h, length, intv = head, 0, 1
+        while h:
+            h, length = h.next, length + 1
+        res = ListNode(0)
+        res.next = head
+        while intv < length:
+            pre, h = res, res.next 
+            while h:
+                h1, i = h, intv
+                while i and h:
+                    h, i = h.next, i - 1
+                if i:
+                    break
+                h2, i = h, intv
+                while i and h:
+                    h, i = h.next, i - 1
+                c1, c2 = intv, intv - i
+                while c1 and c2:
+                    if h1.val < h2.val:
+                        pre.next, h1, c1 = h1, h1.next, c1 - 1
+                    else:
+                        pre.next, h2, c2 = h2, h2.next, c2 - 1
+                    pre = pre.next 
+                pre.next = h1 if c1 else h2
+
+                while c1 > 0 or c2 > 0:
+                    pre, c1, c2 = pre.next, c1 - 1, c2 - 1
+                pre.next = h
+            intv *= 2
+        return res.next
+
+# 22
+class Solution:
+    def getKthFromEnd(self, head: ListNode, k: int) -> ListNode:
+        former, latter = head, head
+        for _ in range(k):
+            former = former.next
+        while former:
+            former, latter = former.next, latter.next 
+        return latter
